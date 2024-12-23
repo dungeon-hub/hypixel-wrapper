0f417ab2e6d755e28955187b98e08c6a5b5de0f8
traible <traible@apollon.de>
2024-12-23 04:22:31 +0100

    Added an expiration time to the cache.

--- src/main/kotlin/net/dungeonhub/hypixel/client/ApiClientWithCache.kt
+++ src/main/kotlin/net/dungeonhub/hypixel/client/ApiClientWithCache.kt
@@ -3,9 +3,14 @@ package net.dungeonhub.hypixel.client
 import net.dungeonhub.cache.Cache
 import net.dungeonhub.hypixel.entities.SkyblockProfiles
 import net.hypixel.api.reply.PlayerReply
+import java.time.Instant
 import java.util.*
 
 interface ApiClientWithCache : ApiClient {
     val playerDataCache : Cache<PlayerReply.Player, UUID>
     val skyblockProfilesCache : Cache<SkyblockProfiles, UUID>
+
+    fun <T : Any> isExpired(cache: Cache<T, UUID>, uuid: UUID, expiresAfterMinutes: Int = 5): Boolean {
+        return cache.retrieveElement(uuid)?.timeAdded?.plusSeconds(expiresAfterMinutes * 60L)?.isBefore(Instant.now()) ?: true
+    }
 }
--- src/main/kotlin/net/dungeonhub/hypixel/client/FallbackApiClient.kt
+++ src/main/kotlin/net/dungeonhub/hypixel/client/FallbackApiClient.kt
@@ -1,10 +1,21 @@
 package net.dungeonhub.hypixel.client
 
+import net.dungeonhub.cache.Cache
 import net.dungeonhub.hypixel.entities.SkyblockProfiles
 import net.hypixel.api.reply.PlayerReply
 import java.util.*
 
-class FallbackApiClient(val first: ApiClient, val second: ApiClient) : ApiClient {
-    override fun getPlayerData(uuid: UUID): PlayerReply.Player? = first.getPlayerData(uuid) ?: second.getPlayerData(uuid)
-    override fun getSkyblockProfiles(uuid: UUID): SkyblockProfiles? = first.getSkyblockProfiles(uuid) ?: second.getSkyblockProfiles(uuid)
+//TODO add tests if values expire correctly
+class FallbackApiClient(val first: ApiClientWithCache, val second: ApiClient, var expiresAfterMinutes: Int = 5) :
+    ApiClient {
+    override fun getPlayerData(uuid: UUID): PlayerReply.Player? =
+        (if (first.playerDataCache.isExpired(uuid)) null else first.getPlayerData(uuid)) ?: second.getPlayerData(uuid)
+
+    override fun getSkyblockProfiles(uuid: UUID): SkyblockProfiles? =
+        (if (first.skyblockProfilesCache.isExpired(uuid)) null else first.getSkyblockProfiles(uuid))
+            ?: second.getSkyblockProfiles(uuid)
+
+    private fun <T : Any> Cache<T, UUID>.isExpired(uuid: UUID): Boolean {
+        return first.isExpired(this, uuid, expiresAfterMinutes)
+    }
 }
