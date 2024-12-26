fccae144acf92178db2a6c267743082078b8b872
Taubsie <50213229+Taubsie@users.noreply.github.com>
2024-12-26 20:58:06 +0100

    Fixed a bug causing certain profiles to not load correctly.

--- src/main/kotlin/net/dungeonhub/hypixel/entities/SlayerBossProgress.kt
+++ src/main/kotlin/net/dungeonhub/hypixel/entities/SlayerBossProgress.kt
@@ -1,17 +1,18 @@
 package net.dungeonhub.hypixel.entities
 
 import com.google.gson.JsonObject
+import net.dungeonhub.provider.getAsJsonObjectOrNull
 import net.dungeonhub.provider.getAsJsonPrimitiveOrNull
 
 class SlayerBossProgress(
-    val claimedLevels: JsonObject,
+    val claimedLevels: JsonObject?,
     val xp: Int?,
     val raw: JsonObject
 )
 
 fun JsonObject.toSlayerProgress(): SlayerBossProgress {
     return SlayerBossProgress(
-        getAsJsonObject("claimed_levels"),
+        getAsJsonObjectOrNull("claimed_levels"),
         getAsJsonPrimitiveOrNull("xp")?.asInt,
         this
     )
