484e54733458780a362d03edf19566d49ff9e4f5
Taubsie <tobias@joerg-hartmann.de>
2025-01-05 01:57:08 +0100

    Bumped the version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 group = "net.dungeon-hub"
 val artifactId = "hypixel-wrapper"
-version = "0.3.6"
+version = "0.4.0"
 description = "A simple Kotlin wrapper for the Hypixel API, including a cache."
 
 repositories {
