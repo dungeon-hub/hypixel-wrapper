c1e52a834e4ff60a894b5522e56fc7ff4faeec0a
Taubsie <tobias@joerg-hartmann.de>
2025-05-10 22:39:17 +0200

    Fixed an incorrect class structure.

--- src/main/kotlin/net/dungeonhub/hypixel/entities/inventory/items/special/Wand.kt
+++ src/main/kotlin/net/dungeonhub/hypixel/entities/inventory/items/special/Wand.kt
@@ -2,6 +2,7 @@ package net.dungeonhub.hypixel.entities.inventory.items.special
 
 import me.nullicorn.nedit.type.NBTCompound
 import net.dungeonhub.hypixel.entities.inventory.items.Gear
+import net.dungeonhub.hypixel.entities.inventory.items.ItemWithCombatBooks
 import net.dungeonhub.hypixel.entities.inventory.items.ManaDisintegratable
 
-open class Wand(raw: NBTCompound) : Gear(raw), ManaDisintegratable
+open class Wand(raw: NBTCompound) : Gear(raw), ManaDisintegratable, ItemWithCombatBooks
