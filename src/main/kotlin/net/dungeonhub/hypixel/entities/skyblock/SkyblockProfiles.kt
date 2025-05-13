2f65dd3e185a4a96d30000f1394e8934bd7ce02b
Taubsie <taubsie@dungeon-hub.net>
2025-05-13 19:36:54 +0200

    Added a calculation for the users Bingo Rank (by searching for the Bingo Pet on the users profile).

--- src/main/kotlin/net/dungeonhub/hypixel/entities/skyblock/SkyblockProfiles.kt
+++ src/main/kotlin/net/dungeonhub/hypixel/entities/skyblock/SkyblockProfiles.kt
@@ -1,8 +1,15 @@
 package net.dungeonhub.hypixel.entities.skyblock
 
-import java.util.UUID
+import net.dungeonhub.hypixel.entities.inventory.SkyblockRarity
+import net.dungeonhub.hypixel.entities.skyblock.misc.ProfileGameMode
+import net.dungeonhub.hypixel.entities.skyblock.pet.KnownPetType
+import java.util.*
 
 class SkyblockProfiles(
     val owner: UUID,
     val profiles: List<SkyblockProfile>
-)
+) {
+    val bingoRank: SkyblockRarity?
+        get() = profiles.filter { it.gameMode == ProfileGameMode.Bingo }.map { it.getCurrentMember(owner) }
+            .flatMap { it?.petsData?.pets ?: emptyList() }.firstOrNull { it.type == KnownPetType.Bingo }?.tier
+}
